dd26f7154dd6b3c39dff859365864d4ccd190e27
Todd Simpson <toddsimpson@BCs-MacBook-Pro.local>
2020-10-15 14:44:53 -0500

    Created Files

---
+++ textGame.js
@@ -0,0 +1,9 @@
+console.log("Connected");
+alert("Welcome to the Ghostbusters Text Adventure Game. Click 'Okay' to start the game.")
+
+let userInput;
+
+prompt("Enter Something");
+
+userInput = prompt("Enter Something");
+alert(userInput);
